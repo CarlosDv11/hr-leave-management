23f452228c40fac1759045a193fe01e48997aca2
CarlosDv11 <carloselima2001@gmail.com>
2026-05-20 22:27:15 -0300

    Add project files.

---
+++ HR.LeaveManagement.Domain/Common/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Domain.Common;
+
+public abstract class BaseEntity
+{
+    public int Id { get; set; }
+    public DateTime? DataCreated { get; set; }
+    public DateTime? DataModified { get; set; }
+}
